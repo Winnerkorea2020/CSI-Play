72b99001a8d2439d3d962f9873598cae0ac39907
Winner2020 <trueater@hanmail.net>
2023-08-22 05:49:23 +0900

    update Design

--- components/ExchangeRate.js
+++ components/ExchangeRate.js
@@ -1,19 +1,25 @@
-import React from "react";
+import Script from "next/script";
 
 const ExchangeRate = () => {
   return (
     <div className="overscroll-none">
-      <iframe
-        height={250}
-        width={500}
-        frameborder="0"
-        allowtransparency="true"
-        marginwidth="0"
-        marginheight="0"
-        src="https://sslfxrates.investing.com/index_exchange.php?params&inner-border-color=%23CBCBCB&border-color=%23cbcbcb&bg1=%23F6F6F6&bg2=%23ffffff&inner-text-color=%23000000&currency-name-color=%23000000&header-text-color=%23FFFFFF&force_lang=18"
-        align="center"
-      ></iframe>
-      <br />
+      <div className="ex-1">
+        <div className="ex-2">
+          <span className="ex-3">
+            <a
+              href="https://www.exchangeratewidget.com/"
+              className="ex-4"
+              rel="nofollow"
+            >
+              US Dollar Exchange Rates
+            </a>
+          </span>
+        </div>
+        <Script
+          type="text/javascript"
+          src="https://www.exchangeratewidget.com/converter.php?l=en&f=USD&t=KRW,CNY,EUR,GBP,JPY,CAD,AUD,HKD,USD,&a=1&d=F0F0F0&n=FFFFFF&o=000000&v=5"
+        ></Script>
+      </div>
     </div>
   );
 };
