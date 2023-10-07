7d05f5d0c0f62513f4caf31e6b4c8b89b001fcc0
Winner2020 <trueater@hanmail.net>
2023-10-07 12:03:52 +0900

    update Design

--- components/BoardDetail.js
+++ components/BoardDetail.js
@@ -7,24 +7,44 @@ const BoardDetail = () => {
     <div>
       <div>
         <div className="pb-5">
-          <h2 className="py-2 font-medium text-lg tracking-tighter truncate whitespace-nowrap">
+          <h2 className="py-2 xl:py-4 xl:text-2xl font-medium text-lg tracking-tighter truncate whitespace-nowrap">
             자유게시판 제목입니다.
           </h2>
-          <div className="flex justify-start items-center ">
-            <div className="pr-5">
-              <span>코인추천뉴스</span>
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-2 items-center">
+            <div className="flex justify-start items-center text-xs ">
+              <div className="pr-3 xl:pr-5">
+                <span className="tracking-tighter">코인추천뉴스</span>
+              </div>
+              <div className="px-3 xl:px-5 border-l-2">
+                <span className="tracking-tighter mr-1">2023-04-02</span>
+                <span className="tracking-tighter ">18:00</span>
+              </div>
+              <div className="px-3 xl:px-5 border-l-2">
+                <span className="tracking-tighter mr-1">댓글</span>
+                <span className="tracking-tighter ">2</span>
+              </div>
+              <div className="px-3 xl:px-5 border-l-2">
+                <span className="tracking-tighter mr-1">추천</span>
+                <span className="tracking-tighter ">135</span>
+              </div>
             </div>
-            <div className="px-5 border-l-2">
-              <span className="tracking-tighter mr-1">2023-04-02</span>
-              <span className="tracking-tighter ">18:00</span>
-            </div>
-            <div className="px-5 border-l-2">
-              <span className="tracking-tighter mr-1">댓글</span>
-              <span className="tracking-tighter ">2</span>
-            </div>
-            <div className="px-5 border-l-2">
-              <span className="tracking-tighter mr-1">추천</span>
-              <span className="tracking-tighter ">135</span>
+            <div className="flex justify-start xl:justify-end items-center gap-2">
+              <button
+                type="button"
+                className=" py-1.5 px-2 border rounded-full hover:bg-red-500 hover:text-white"
+              >
+                <div className="flex justify-center items-center gap-1">
+                  <BiLike size={15} /> <div>추천하기</div>
+                </div>
+              </button>
+              <button
+                type="button"
+                className=" py-1.5 px-2 border rounded-full hover:bg-blue-500 hover:text-white"
+              >
+                <div className="flex justify-center items-center gap-1">
+                  <BiShare size={15} /> <div>공유하기</div>
+                </div>
+              </button>
             </div>
           </div>
         </div>
@@ -32,17 +52,41 @@ const BoardDetail = () => {
         <div>
           <div className="mt-4">
             <p>
-              Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-              in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-              Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-              consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,
-              discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-              Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
-              on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum
-              dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the
-              1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
-              Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from
-              the 1914 translation by H. Rackham.
+              Where does it come from? Contrary to popular belief, Lorem Ipsum
+              is not simply random text. It has roots in a piece of classical
+              Latin literature from 45 BC, making it over 2000 years old.
+              Richard McClintock, a Latin professor at Hampden-Sydney College in
+              Virginia, looked up one of the more obscure Latin words,
+              consectetur, from a Lorem Ipsum passage, and going through the
+              cites of the word in classical literature, discovered the
+              undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+              1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
+              and Evil) by Cicero, written in 45 BC. This book is a treatise on
+              the theory of ethics, very popular during the Renaissance. The
+              first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+              from a line in section 1.10.32. The standard chunk of Lorem Ipsum
+              used since the 1500s is reproduced below for those interested.
+              Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum"
+              by Cicero are also reproduced in their exact original form,
+              accompanied by English versions from the 1914 translation by H.
+              Rackham.Where does it come from? Contrary to popular belief, Lorem
+              Ipsum is not simply random text. It has roots in a piece of
+              classical Latin literature from 45 BC, making it over 2000 years
+              old. Richard McClintock, a Latin professor at Hampden-Sydney
+              College in Virginia, looked up one of the more obscure Latin
+              words, consectetur, from a Lorem Ipsum passage, and going through
+              the cites of the word in classical literature, discovered the
+              undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+              1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
+              and Evil) by Cicero, written in 45 BC. This book is a treatise on
+              the theory of ethics, very popular during the Renaissance. The
+              first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+              from a line in section 1.10.32. The standard chunk of Lorem Ipsum
+              used since the 1500s is reproduced below for those interested.
+              Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum"
+              by Cicero are also reproduced in their exact original form,
+              accompanied by English versions from the 1914 translation by H.
+              Rackham.
             </p>
           </div>
         </div>
@@ -70,23 +114,6 @@ const BoardDetail = () => {
               className=" py-1.5 px-2 border bg-gray-600 hover:bg-yellow-600 rounded-xl  tracking-tighter text-xs w-full  cursor-pointer  text-white"
             >
               <div>목록</div>
-            </button>
-
-            <button
-              type="button"
-              className=" py-1.5 px-2 border bg-gray-600 hover:bg-yellow-600 rounded-xl  tracking-tighter text-xs w-full  cursor-pointer  text-white"
-            >
-              <div className="flex justify-center items-center gap-1">
-                <BiLike size={15} /> <div>추천하기</div>
-              </div>
-            </button>
-            <button
-              type="button"
-              className=" py-1.5 px-2 border bg-gray-600 hover:bg-yellow-600 rounded-xl  tracking-tighter text-xs w-full  cursor-pointer  text-white"
-            >
-              <div className="flex justify-center items-center gap-1">
-                <BiShare size={15} /> <div>공유하기</div>
-              </div>
             </button>
           </div>
         </div>
