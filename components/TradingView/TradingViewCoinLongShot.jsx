c161cbae5ae24cabdcd45300b2c86ccb9787cb10
Winner2020 <68554587+Winnerkorea2020@users.noreply.github.com>
2023-11-26 18:59:05 +0900

    Update TradingViewCoinLongShot.jsx

--- components/TradingView/TradingViewCoinLongShot.jsx
+++ components/TradingView/TradingViewCoinLongShot.jsx
@@ -1,48 +1,7 @@
-// TradingViewCoinLongShot.jsx
-"use client";
+import React from "react";
 
-import React, { useEffect, useRef } from "react";
+const TradingViewCoinLongShot = () => {
+  return <div>TradingViewCoinLongShot</div>;
+};
 
-let tvScriptLoadingPromise;
-
-export default function TradingViewCoinLongShot() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-screener.js";
-    script.async = true;
-    script.innerHTML = JSON.stringify({
-      width: "100%",
-      height: "490",
-      defaultColumn: "overview",
-      screener_type: "crypto_mkt",
-      displayCurrency: "USD",
-      colorTheme: "light",
-      locale: "kr",
-    });
-    document
-      .getElementsByClassName("tradingview-widget-container__widget")[0]
-      .appendChild(script);
-
-    return () => {
-      document
-        .getElementsByClassName("tradingview-widget-container__widget")[0]
-        .removeChild(script);
-    };
-  }, []);
-
-  return (
-    <div className="tradingview-widget-container">
-      <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://kr.tradingview.com/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">트레이딩뷰에서 모든 시장 추적</span>
-        </a>
-      </div>
-    </div>
-  );
-}
+export default TradingViewCoinLongShot;
