b8bf5c7c858199f3920b1316b1d714611727273f
Winner2020 <68554587+Winnerkorea2020@users.noreply.github.com>
2023-11-28 20:39:59 +0900

    update Design

---
+++ app/(main)/coin/coinFreeBoard/page.jsx
@@ -0,0 +1,12 @@
+import TableList from "@/components/table/TableList";
+
+const CoinFreeBoard = () => {
+  const title = "코인 자유게시판";
+  return (
+    <div className="h-full bg-white">
+      <TableList title={title} />
+    </div>
+  );
+};
+
+export default CoinFreeBoard;
